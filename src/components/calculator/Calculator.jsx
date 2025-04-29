b46fea7f9ea71f6b062601331f386c54a5fed4b3
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-04-29 23:02:06 +0300

    Create Result component

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,9 +1,11 @@
 import CalculatorForm from "./calculator-form/CalculatorForm";
+import Results from "./results/Results.jsx";
 
 export default function Calculator() {
     return (
         <div className="calculator-container">
             <CalculatorForm />
+            <Results />
         </div>
     );
 };
---
+++ src/components/calculator/results/Results.jsx
@@ -0,0 +1,15 @@
+export default function Results() {
+    return (
+        <div className="results-container">
+            <h2 className="results-heading">Results:</h2>
+            <div className="results-info">
+                <p>Body fats (%): </p>
+                <p>Body fats (kg): </p>
+                <p>Lean body mass: </p>
+                <p>Basal metabolic rate (BMR): </p>
+                <p>Daily calorie intake: </p>
+                <p>Daily calorie intake deficit/excess: </p>
+            </div>
+        </div>
+    );
+};

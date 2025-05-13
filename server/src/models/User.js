34bd3c582dcaf7a3f59580585ed2442329c29897
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-05-13 23:48:58 +0300

    Create User model

---
+++ server/src/models/User.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt';
+
+const SALT_ROUNDS = 10;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Email is required!'],
+        minLength: [8, 'Email should be at least 8 characters long']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!'],
+        minLength: [4, 'Password should be at least 4 characters long']
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, SALT_ROUNDS);
+    this.password = hash;
+})
+
+const User = model('User', userSchema);
+
+export default User;

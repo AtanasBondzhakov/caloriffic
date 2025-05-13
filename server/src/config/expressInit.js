a7d88ecd8f10d4ef1296a3e8c565fb6cf3c71608
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-05-13 23:35:11 +0300

    Create index.js and routes files

--- server/src/config/expressInit.js
+++ server/src/config/expressInit.js
@@ -1,5 +1,8 @@
 import { json } from "express";
 
+import routes from "../routes.js";
+
 export default function expressInit(app) {
     app.use(json());
+    app.use(routes);
 };
---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+import mongooseInit from './config/mongooseInit.js';
+import expressInit from './config/expressInit.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+mongooseInit();
+expressInit(app);
+
+app.listen(PORT, () => console.log(`Server is now running on http://localhost:${PORT}...`));

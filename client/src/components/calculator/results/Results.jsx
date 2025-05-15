b8d89070c79b3d81092429bb306fea06a23c24c3
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-05-16 00:23:04 +0300

    Refactor Results with accessing data by props

--- client/src/components/calculator/results/Results.jsx
+++ client/src/components/calculator/results/Results.jsx
@@ -1,15 +1,19 @@
-export default function Results() {
+export default function Results({
+    results,
+    handleSave
+}) {
     return (
         <div className="results-container">
             <h2 className="results-heading">Results:</h2>
             <div className="results-info">
-                <p>Body fats (%): </p>
-                <p>Body fats (kg): </p>
-                <p>Lean body mass: </p>
-                <p>Basal metabolic rate (BMR): </p>
-                <p>Daily calorie intake: </p>
+                <p>Body fats (%): {results.bmi ? results.bmi?.toFixed(1) : 0} %</p>
+                <p>Body fats (kg): {results.bfk ? results.bfk?.toFixed(1) : 0} kg</p>
+                <p>Lean body mass: {results.lbm ? results.lbm?.toFixed(1) : 0} kg</p>
+                <p>Basal metabolic rate (BMR): {Math.round(results?.bmr)} kcal</p>
+                <p>Daily calorie intake: {Math.round(results?.dci)} kcal</p>
                 <p>Daily calorie intake deficit/excess: </p>
             </div>
+            <button onClick={handleSave}>Save</button>
         </div>
     );
 };

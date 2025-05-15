6dc8f37c008c4be3a3b6afdd1853a5c403cc108c
atanasbondzhakov <atanas.b@yahoo.com>
2025-05-15 14:23:19 +0300

    Create store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { Provider } from 'react-redux';
+
+import App from './App.jsx'
 import './styles.css';
+import store from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </StrictMode>,
 )

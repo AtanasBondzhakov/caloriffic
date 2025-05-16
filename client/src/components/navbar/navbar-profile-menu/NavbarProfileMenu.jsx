1483e47d211ec1a8bd0b29cb02817af5f27b4532
atanasbondzhakov <atanas.b@yahoo.com>
2025-05-16 15:38:29 +0300

    Update navbar profile menu to handle logout

--- client/src/components/navbar/navbar-profile-menu/NavbarProfileMenu.jsx
+++ client/src/components/navbar/navbar-profile-menu/NavbarProfileMenu.jsx
@@ -1,32 +1,52 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Dropdown, Space } from 'antd';
 import { FaRegUserCircle } from "react-icons/fa";
-import { Link } from 'react-router';
-const items = [
-    {
-        key: '1',
-        label:(
-            <Link to='/auth/login' style={{fontSize: '1.2em', padding: '0.5em'}}>
-                Login
-            </Link>
-        ),
-    },
-    {
-        key: '2',
-        label:(
-            <Link to='/auth/register' style={{fontSize: '1.2em', padding: '0.5em'}}>
-                Register
-            </Link>
-        ),
-    }
-];
+import { Link, useNavigate } from 'react-router';
+import { logoutUser } from '../../../store/slices/authSlice';
 
 export default function NavbarProfileMenu() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const logoutHandle = async () => {
+        await dispatch(logoutUser({ navigate })).unwrap();
+    };
+
+    const { isAuthenticated } = useSelector(state => state.auth);
+
+    const items = [
+        !isAuthenticated && {
+            key: '1',
+            label: (
+                <Link to='/auth/login' style={{ fontSize: '1.2em', padding: '0.5em' }}>
+                    Login
+                </Link>
+            ),
+        },
+        !isAuthenticated && {
+            key: '2',
+            label: (
+                <Link to='/auth/register' style={{ fontSize: '1.2em', padding: '0.5em' }}>
+                    Register
+                </Link>
+            ),
+        },
+        isAuthenticated && {
+            key: '3',
+            label: (
+                <span style={{ fontSize: '1.2em', padding: '0.5em', cursor: 'pointer' }} onClick={logoutHandle}>
+                    Logout
+                </span>
+            ),
+        }
+    ];
+
     return (
-        <Dropdown menu={{ items }} placement='bottomLeft' className='nav-item'>
+        <Dropdown menu={{ items }} placement='bottomLeft' className='dropdown'>
             <a onClick={e => e.preventDefault()}>
                 <Space>
-                    <FaRegUserCircle size='1.5em' color='#080808'/>
+                    <FaRegUserCircle size='1.5em' color='#080808' />
                 </Space>
             </a>
         </Dropdown>

d8254d146e33a51e9043a165b8a76804258fbebf
atanasbondzhakov <atanas.b@yahoo.com>
2025-05-22 13:36:35 +0300

    Add loginSchema with yup

---
+++ client/src/schema/loginSchema.js
@@ -0,0 +1,6 @@
+import { object, string } from 'yup';
+
+export const loginSchema = object().shape({
+    'email': string().email('Invalid email format').required('Email is required'),
+    'password': string().required('Password is required'),
+});

487ce5993e8bbbf4fec15e29be674b1142b18618
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-05-26 23:51:27 +0300

    Convert Calculator component to use CSS Modules

--- client/src/components/calculator/Calculator.jsx
+++ client/src/components/calculator/Calculator.jsx
@@ -1,15 +1,16 @@
 import CalculatorForm from "./calculator-form/CalculatorForm";
 import Results from "./results/Results.jsx";
+import styles from '../calculator/Calculator.module.css';
 
 export default function Calculator() {
     return (
-        <div className="calculator-container">
-            <div className="calculator-header">
+        <div className={styles['calculator-container']}>
+            <div className={styles['calculator-header']}>
                 <h1>Calorie Calculator</h1>
                 <p>Calculate your daily caloric needs based on your measurements.</p>
             </div>
-            <div className="calculator-content">
-                <img src="/bodies.png" alt="" className="body-image" />
+            <div className={styles['calculator-content']}>
+                <img src="/bodies.png" alt="" className={styles['body-image']} />
                 <CalculatorForm />
             </div>
         </div>

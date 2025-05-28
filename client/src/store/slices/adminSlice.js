c91a00463d93de425f49d48b75da23005c281f27
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-05-28 23:28:11 +0300

    Create adminSlice with getAllUsers functionality

---
+++ client/src/store/slices/adminSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import requester from "../../api/requester.js";
+
+export const getAllUsers = createAsyncThunk('admin/manage-profiles', async (_, { rejectWithValue }) => {
+    try {
+        const users = await requester.get('/admin/manage-profiles');
+        
+        return users;
+    } catch (err) {
+        return rejectWithValue(err);
+    }
+})
+
+const initialState = {
+    users: [],
+    selectedUser: null,
+    loading: false,
+    error: null,
+    updateStatus: 'idle',
+    updateError: null
+}
+
+const adminSlice = createSlice({
+    name: 'admin',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAllUsers.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(getAllUsers.fulfilled, (state, action) => {
+                state.loading = false;
+                state.users = action.payload;
+            })
+            .addCase(getAllUsers.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.message;
+            })
+    }
+})
+
+export default adminSlice;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authSlice from './slices/authSlice';
 import bodyMetricsSlice from './slices/bodyMetricsSlice.js';
+import adminSlice from './slices/adminSlice.js';
 
 const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
-        bodyMetrics: bodyMetricsSlice.reducer
+        bodyMetrics: bodyMetricsSlice.reducer,
+        admin: adminSlice.reducer
     }
 });
 

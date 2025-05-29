629beb9a6202b22c8f64b04c7abe1f72e8323f2b
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-05-29 23:54:37 +0300

    Add Dialog modal for delete user

--- client/src/components/admin/manage-users/ManageUsers.jsx
+++ client/src/components/admin/manage-users/ManageUsers.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import styles from './ManageUsers.module.css';
@@ -8,10 +8,23 @@ import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHe
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import dateFormatter from "../../../utils/dateFormatter.js";
+import DialogModal from "../../ui/dialog-modal/DialogModal.jsx";
 
 export default function ManageUsers() {
     const dispatch = useDispatch();
     const { users } = useSelector(state => state.admin);
+
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModalHandler = (user) => {
+        setSelectedUser(user);
+        setIsModalOpen(true);
+    };
+
+    const closeModalHandler = () => {
+        setIsModalOpen(false);
+    };
 
     useEffect(() => {
         dispatch(getAllUsers());
@@ -19,17 +32,19 @@ export default function ManageUsers() {
 
     const deleteUserHandler = (userId) => {
         dispatch(deleteUser(userId));
+        setIsModalOpen(false);
     };
 
     return (
         <div className={styles.container}>
+            {isModalOpen && <DialogModal onConfirm={() => deleteUserHandler(selectedUser._id)} onClose={closeModalHandler} email={selectedUser.email} />}
+
             <div className={styles.heading}><h2>Heading</h2></div>
             <div className={styles.table}>
                 <TableContainer component={Paper}>
                     <Table>
                         <TableHead sx={{ backgroundColor: '#5380bb' }}>
                             <TableRow>
-                                <TableCell align="center"><input type="checkbox" /></TableCell>
                                 <TableCell align="center" sx={{ color: "white" }}>ID</TableCell>
                                 <TableCell align="center" sx={{ color: "white" }}>Email</TableCell>
                                 <TableCell align="center" sx={{ color: "white" }}>Role</TableCell>
@@ -38,21 +53,20 @@ export default function ManageUsers() {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {users.map(row => (
+                            {users.map(user => (
                                 <TableRow
-                                    key={row._id}
+                                    key={user._id}
                                 >
-                                    <TableCell align="center" sx={{ width: '5%', whiteSpace: 'nowrap' }}><input type="checkbox" /></TableCell>
-                                    <TableCell align="center" sx={{ width: 'auto' }}>{row._id}</TableCell>
-                                    <TableCell align="center" width={'auto'}>{row.email}</TableCell>
-                                    <TableCell align="center" sx={{ width: 'auto' }}>{row.role}</TableCell>
-                                    <TableCell align="center" sx={{ width: 'auto' }}>{dateFormatter(row.createdAt)}</TableCell>
+                                    <TableCell align="center" sx={{ width: 'auto' }}>{user._id}</TableCell>
+                                    <TableCell align="center" width={'auto'}>{user.email}</TableCell>
+                                    <TableCell align="center" sx={{ width: 'auto' }}>{user.role}</TableCell>
+                                    <TableCell align="center" sx={{ width: 'auto' }}>{dateFormatter(user.createdAt)}</TableCell>
                                     <TableCell align="center" sx={{ width: '10%', whiteSpace: 'nowrap' }}>
                                         <IconButton>
                                             <EditOutlinedIcon />
                                         </IconButton>
                                         <IconButton>
-                                            <DeleteOutlineOutlinedIcon onClick={() => deleteUserHandler(row._id)} />
+                                            <DeleteOutlineOutlinedIcon onClick={() => openModalHandler(user)} />
                                         </IconButton>
                                     </TableCell>
                                 </TableRow>

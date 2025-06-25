111721d0432c49bcde27ee69e41283f32583d119
atanasbondzhakov <atanas.b@yahoo.com>
2025-06-25 15:22:27 +0300

    Udate product controller with trycatch block

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -4,16 +4,23 @@ import { productService } from "../services/productService.js";
 
 const productController = Router();
 
-productController.get('/:productName', async (req, res) => {
-    const query = req.params.productName;
+productController.get('/', async (req, res) => {
+    const query = req.query.search;
 
     if (!query) {
         return res.status(400).json({ success: false, message: "Missing product name parameter." });
     }
 
-    const products = await productService.getProduct(query);
+    try {
+        const products = await productService.getProduct(query);
 
-    return products
+        return res.status(200).json(products);
+    } catch (err) {
+        console.log('Error in search products', err);
+
+        return res.status(500).json({ success: false, message: 'Server Error' });
+    }
+
 
 });
 

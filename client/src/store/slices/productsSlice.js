c361f039a5733bc94a8985f5d676aeb9f4f88f41
atanasbondzhakov <atanas.b@yahoo.com>
2025-07-01 15:08:11 +0300

    Add getProductById thunk

--- client/src/store/slices/productsSlice.js
+++ client/src/store/slices/productsSlice.js
@@ -4,8 +4,19 @@ import requester from "../../api/requester";
 export const getProducts = createAsyncThunk('products', async (productName, { rejectWithValue }) => {
     try {
         const products = await requester.get(`/products?search=${productName}`);
-        
+
         return products;
+    } catch (err) {
+        return rejectWithValue(err);
+    }
+})
+
+export const getProductById = createAsyncThunk('products/product', async (productId, { rejectWithValue }) => {
+    try {
+        const product = await requester.get(`/products/${productId}`);
+        console.log(productId);
+
+        return product;
     } catch (err) {
         return rejectWithValue(err);
     }
@@ -34,6 +45,18 @@ const productsSlice = createSlice({
                 state.products = action.payload;
             })
             .addCase(getProducts.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.message;
+            })
+            .addCase(getProductById.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(getProductById.fulfilled, (state, action) => {
+                state.loading = false;
+                state.selected = action.payload;
+            })
+            .addCase(getProductById.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload.message;
             })

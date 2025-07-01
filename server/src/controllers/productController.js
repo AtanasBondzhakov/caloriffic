6e20546b97c045f563089fd3a3f0c1b4b42a6196
atanasbondzhakov <atanas.b@yahoo.com>
2025-07-01 15:12:04 +0300

    Add productById functionality in controller

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -8,12 +8,13 @@ productController.get('/', async (req, res) => {
     const query = req.query.search;
 
     if (!query) {
+
         const allProducts = await productService.getAllProducts();
         return res.status(200).json(allProducts);
     }
 
     try {
-        const products = await productService.getProduct(query);
+        const products = await productService.getProducts(query);
 
         return res.status(200).json(products);
     } catch (err) {
@@ -21,8 +22,16 @@ productController.get('/', async (req, res) => {
 
         return res.status(500).json({ success: false, message: 'Server Error' });
     }
+});
 
-
+productController.get('/:productId', async (req, res) => {
+    try {
+        const product = productService.getProductById(req.params.productId);
+        return res.status(200).json(product);
+    } catch (err) {
+        console.log('Error in fetch product', err);
+        return res.status(500).json({ success: false, message: 'Server Error' });
+    }
 });
 
 export default productController;

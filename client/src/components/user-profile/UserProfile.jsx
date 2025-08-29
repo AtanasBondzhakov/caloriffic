84930be32f63f87b9daca102084ab6db0f778670
atanasbondzhakov <atanas.b@yahoo.com>
2025-08-29 15:53:40 +0300

    Add style changes on Pie Chart

--- client/src/components/user-profile/UserProfile.jsx
+++ client/src/components/user-profile/UserProfile.jsx
@@ -1,8 +1,10 @@
 import styles from "../user-profile/UserProfile.module.css";
 import CustomButton from "../ui/custom-button/CustomButton";
 import CustomPieChart from "../ui/custom-pie-chart/CustomPieChart";
+import { useSelector } from "react-redux";
 
 export default function UserProfile() {
+    const { user } = useSelector(state => state.auth);
 
     return (
         <div className={styles.container}>
@@ -13,19 +15,19 @@ export default function UserProfile() {
                         <p>Daily Progress</p>
                     </div>
                     <div className={styles.pie}>
-                        <CustomPieChart />
+                        <CustomPieChart user={user} />
                         <p>Pie Chart</p>
                     </div>
                 </div>
                 <div className={styles['right-section']}>
-                   
-                        <div className={styles['daily-total']}>
-                            <p>List of Daily Income Calories</p>
-                        </div>
-                        <div className={styles.buttons}>
-                            <CustomButton label="Add Product" />
-                        </div>
-                   
+
+                    <div className={styles['daily-total']}>
+                        <p>List of Daily Income Calories</p>
+                    </div>
+                    <div className={styles.buttons}>
+                        <CustomButton label="Add Product" />
+                    </div>
+
                 </div>
             </div>
         </div>

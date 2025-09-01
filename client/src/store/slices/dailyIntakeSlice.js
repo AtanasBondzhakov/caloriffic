54af895373322c03cbb3e57095886efe287f0975
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-09-01 23:21:50 +0300

    Create dailyIntakeSlice

---
+++ client/src/store/slices/dailyIntakeSlice.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import requester from "../../api/requester.js";
+
+export const addProductToDaily = createAsyncThunk('dailyIntake/addProductToDaily', async ({ productId, quantity }, { rejectedWithValue }) => {
+    try {
+        const result = await requester.post(`/products/add-product-to-daily/${productId}`);
+
+        return result;
+    } catch (err) {
+        return rejectedWithValue(err);
+    }
+});
+
+const initialState = {
+    today: null,
+    loading: false,
+    error: null
+};
+
+const dailyIntakeSlice = createSlice({
+    name: 'dailyIntake',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(addProductToDaily.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(addProductToDaily.fulfilled, (state, action) => {
+                state.loading = false;
+                state.today = action.payload;
+            })
+            .addCase(addProductToDaily.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.message;
+            })
+    }
+});
+
+export default dailyIntakeSlice;

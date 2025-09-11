0543ddd28eb9060f4ca5870c93a7aa59276a9b80
atanasbondzhakov <atanas.b@yahoo.com>
2025-09-11 14:21:26 +0300

    Add loading indicator for selected product details

--- client/src/components/products/product-details/ProductDetails.jsx
+++ client/src/components/products/product-details/ProductDetails.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+
+import styles from './ProductDetails.module.css';
 import Input from '../../forms/input/Input';
 import CustomButton from '../../ui/custom-button/CustomButton';
-import styles from './ProductDetails.module.css';
 import Spinner from '../../ui/spinner/Spinner.jsx';
 
 export default function ProductDetails({
@@ -12,11 +14,35 @@ export default function ProductDetails({
 }) {
     const { loadingSelected } = useSelector(state => state.products);
 
-    if (loadingSelected) {
-        return <Spinner />
-    }
+    const previousSelectedRef = useRef(null);
+    const [showSpinner, setShowSpinner] = useState(false);
 
-    if (!selected) {
+    useEffect(() => {
+        if (selected) {
+            previousSelectedRef.current = selected;
+        }
+    }, [selected]);
+
+    useEffect(() => {
+        let timeout;
+
+        if (loadingSelected) {
+            timeout = setTimeout(() => {
+                setShowSpinner(true)
+            }, 300)
+
+        } else {
+            setShowSpinner(false);
+        }
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [loadingSelected, showSpinner]);
+
+    const displayProduct = loadingSelected ? previousSelectedRef.current : selected;
+
+    if (!displayProduct) {
         return (
             <div className={styles['no-product']}>
                 <h3 className={styles['not-selected']}>No product selected yet.</h3>
@@ -33,32 +59,34 @@ export default function ProductDetails({
 
     return (
         <div className={styles['product-result']}>
-            <div className={styles['product-info']}>
-                <div className={styles['product-heading']}>
-                    <h2>{selected.name}</h2>
-                    <p>Information per 100g</p>
-                </div>
-                <div className={styles['product-nutri']}>
-                    {nutrients.map(n => (
-                        <p key={n.key}>
-                            {n.label}: <span>{selected[n.key]}</span>
-                        </p>
-                    ))}
-                </div>
-                <Input
-                    className={styles['input-form']}
-                    type='number'
-                    name='quantity'
-                    onChange={handleChange}
-                    value={values.quantity}
-                    label='Intake Quantity / g'
-                />
-                <CustomButton
-                    className={styles['custom-btn']}
-                    label='Add Product'
-                    handleClick={handleSubmit}
-                />
-            </div>
+            {showSpinner ? <Spinner />
+                : (
+                    <div className={styles['product-info']}>
+                        <div className={styles['product-heading']}>
+                            <h2>{selected.name}</h2>
+                            <p>Information per 100g</p>
+                        </div>
+                        <div className={styles['product-nutri']}>
+                            {nutrients.map(n => (
+                                <p key={n.key}>
+                                    {n.label}: <span>{selected[n.key]}</span>
+                                </p>
+                            ))}
+                        </div>
+                        <Input
+                            className={styles['input-form']}
+                            type='number'
+                            name='quantity'
+                            onChange={handleChange}
+                            value={values.quantity}
+                            label='Intake Quantity / g'
+                        />
+                        <CustomButton
+                            className={styles['custom-btn']}
+                            label='Add Product'
+                            handleClick={handleSubmit}
+                        />
+                    </div>)}
         </div>
     );
 };

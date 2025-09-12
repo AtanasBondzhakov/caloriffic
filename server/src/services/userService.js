a51c4b81cd77f04f1e67eb80cdf1a986cac4be92
atanasbondzhakov <atanas.b@yahoo.com>
2025-09-12 15:36:11 +0300

    Create user service

---
+++ server/src/services/userService.js
@@ -0,0 +1,17 @@
+import DailyIntake from "../models/DailyIntake.js";
+
+export const userService = {
+    async getDailyIntake(userId) {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const todayString = String(today.getDate()).padStart(2, '0') + '-' +
+            String(today.getMonth() + 1).padStart(2, '0') + '-' +
+            today.getFullYear();
+
+        const dailyIntake = await DailyIntake.findOne({ owner: userId, date: todayString }).lean();
+        console.log('daily intake:', dailyIntake);
+
+        return dailyIntake;
+    }
+};
